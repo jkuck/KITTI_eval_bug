c6210023ec6f57f7030bcc4c75aa2a947daca521
jkuck <jdkuck@gmail.com>
2016-10-02 21:08:38 -0700

    Update compare_scripts.sh

--- compare_scripts.sh
+++ compare_scripts.sh
@@ -3,6 +3,8 @@
 echo "Results from the current evaluation script run on ground truth with don't cares:"
 python current_evaluate_tracking.py test_eval_on_ground_truth
 
+echo ""
+echo "------------------------------------------------------------------------------------"
 echo "Results from the proposed evaluation script run on ground truth with don't cares:"
 python proposed_evaluate_tracking.py test_eval_on_ground_truth
 
